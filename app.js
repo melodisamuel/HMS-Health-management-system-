36e8cb4db40e71abcd470e2438f907c8aea97a90
Melody Samuel <Melodysamuel126@gmail.com>
2024-01-30 12:15:39 +0100

    Implemented the rate limliting function to prevent brute force attacks

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit')
 
 const AppError = require('./utils/appError')
 const ErrorHandler = require('./controllers/errorController');
@@ -14,6 +15,13 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, Please try again in an hour'
+})
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
