c2b23ddd20447cbc7020ca3a6e664455750e7e41
Melody Samuel <Melodysamuel126@gmail.com>
2024-02-13 11:45:54 +0100

    Receptionist can now view nurses schedule

--- controllers/receptionistController.js
+++ controllers/receptionistController.js
@@ -1,5 +1,6 @@
 const Appointment = require('../models/appointment');
 const catchAsync = require('../utils/catchAsync');
+const Registration = require('../models/registration');
 
 exports.viewAppointmentList = catchAsync(async (req, res, next) => {
     const appointment = await Appointment.find()
@@ -8,6 +9,17 @@ exports.viewAppointmentList = catchAsync(async (req, res, next) => {
         status: 'success',
         data: {
             appointment
+        }
+    })
+});
+
+exports.viewNursesSchedule = catchAsync(async (req, res, next) => {
+    const nurses = await Registration.find({ role: "nurse" })
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            nurses
         }
     })
 })
--- routes/receptionistRoutes.js
+++ routes/receptionistRoutes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const receptionistController = require('../controllers/receptionistController');
 const authController = require('../controllers/authController');
 
+
 const router = express.Router()
 
 router.route('/view-appointment-list').get(authController.protect, receptionistController.viewAppointmentList);
+router.route('/view-nurses-schedule').get(authController.protect, receptionistController.viewNursesSchedule);
 
 module.exports = router;
