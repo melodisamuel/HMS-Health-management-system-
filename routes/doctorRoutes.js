71aa4a95a2dda0ae075fee9e6bf4aa5651583632
Melody Samuel <Melodysamuel126@gmail.com>
2024-03-04 15:11:45 +0100

    Doctor can now manage profile

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.route('/view-schedule/:id').get(authController.protect, authController.restrictTo('admin', 'doctor', 'nurse'), doctorController.viewSchedule);
 router.route('/view-schedule').get(authController.protect, authController.restrictTo('admin', 'doctor', 'nurse'), doctorController.viewSchedule);
-
+router.route('/track-examination-result').get(authController.protect, authController.restrictTo('admin', 'doctor'), doctorController.trackExaminationResult);
 router.route('/view-nurses-schedule/:id').get(authController.protect, authController.restrictTo('admin', 'doctor', 'nurse'), doctorController.viewNursesSchedule);
 router.route('/view-labAssistant-schedule/:id').get(authController.protect, authController.restrictTo('admin', 'doctor', 'lab assistant'), doctorController.viewLabAssistantSchedule);
 
@@ -14,7 +14,8 @@ router.route('/enter-prescription').post(authController.protect, doctorControlle
 router.route('/generate-reports-results').post(authController.protect, doctorController.generateResultsAndReports);
 router.route('/diagnose-patient').post(authController.protect, authController.restrictTo('admin', 'doctor'), doctorController.diagnosePatient);
 router.route('/recommend-examination').post(authController.protect, authController.restrictTo('admin', 'doctor'), doctorController.recommendExamination);
-router.route('/manage-profile').patch(authController.protect, authController.restrictTo('admin', 'doctor'), doctorController.manageProfile);
-router.route('/track-examination-result').get(authController.protect, authController.restrictTo('admin', 'doctor'), doctorController.trackExaminationResult);
+
+router.route('/manage-profile/:id').put(authController.protect, authController.restrictTo('admin', 'doctor'), doctorController.manageProfile);
+
 
 module.exports = router;
