5bcdbfdec6b950619103d7b2d02a6f2d44388cb7
Melody Samuel <Melodysamuel126@gmail.com>
2024-03-05 10:07:30 +0100

    Implemnted the function for nurses to assign doctors to concerned patients

--- controllers/nurseController.js
+++ controllers/nurseController.js
@@ -1,4 +1,5 @@
 const PreAssessment = require('../models/preAssessment');
+const NurseAssignment = require('../models/assignDoctor');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
@@ -11,6 +12,19 @@ exports.patientPreAssessment = catchAsync(async (req, res, next) => {
         results: patient.length,
         data: {
             patient
+        }
+    })
+});
+
+exports.assignDoctor = catchAsync(async (req, res, next) => {
+    const doctor = await NurseAssignment.create(req.body);
+
+    res.status(201).json({
+        status: 'success',
+        message: 'Doctor has been assigned',
+        results: doctor.length,
+        data: {
+            doctor
         }
     })
 })
