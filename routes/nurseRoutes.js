2c0ff21baa1f28b9d0ba58beb54eaf98388e337f
Melody Samuel <Melodysamuel126@gmail.com>
2024-03-05 12:00:44 +0100

    Handled the route for nurses to collect specific

--- routes/nurseRoutes.js
+++ routes/nurseRoutes.js
@@ -7,5 +7,7 @@ const router = express.Router();
 router.route('/patient-preAssessment').post(authController.protect, authController.restrictTo('admin', 'nurse'), nurseController.patientPreAssessment);
 router.route('/assign-doctor').post(authController.protect, authController.restrictTo('admin', 'nurse'), nurseController.assignDoctor);
 
+router.route('/collect-specimen').post(authController.protect, authController.restrictTo('admin', 'nurse'), nurseController.collectSpecimen);
+
 
 module.exports = router
