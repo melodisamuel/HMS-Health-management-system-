f17d136e4841e418d9c42af1d566126d3330a369
Melody Samuel <Melodysamuel126@gmail.com>
2024-03-05 09:54:17 +0100

    Created a schema for nurses to assign concerned doctors to patient

---
+++ models/assignDoctor.js
@@ -0,0 +1,24 @@
+const NurseAssignmentSchema = new mongoose.Schema({
+    nurse: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Nurse',
+        required: true
+    },
+    patient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Patient',
+        required: true
+    },
+    assignedDoctor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Doctor',
+        required: true
+    },
+    assignmentDate: {
+        type: Date,
+        default: Date.now
+    },
+    // Other assignment details can be added as needed
+});
+
+const NurseAssignment = mongoose.model('NurseAssignment', NurseAssignmentSchema);
