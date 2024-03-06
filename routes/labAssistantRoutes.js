56991e6675853c58c029145774166f4ac12f9fba
Melody Samuel <Melodysamuel126@gmail.com>
2024-03-06 14:45:52 +0100

    Handled the route for lab assisstant to process specimen

--- routes/labAssistantRoutes.js
+++ routes/labAssistantRoutes.js
@@ -5,6 +5,7 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.route('/process-examination').post(authController.protect, authController.restrictTo('admin', 'lab assistant'), labAssistantController.ProcessResult);
+router.route('/process-specimen').post(authController.protect, authController.restrictTo('admin', 'lab assistant'), labAssistantController.processSpecimen);
 
 
 module.exports = router;
