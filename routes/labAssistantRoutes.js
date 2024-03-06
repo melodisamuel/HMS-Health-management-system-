424c9744e9a036d26d514671a2da30b32e3f3551
Melody Samuel <Melodysamuel126@gmail.com>
2024-03-06 15:03:39 +0100

    Handled route for lab assisstant to update profile

--- routes/labAssistantRoutes.js
+++ routes/labAssistantRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.route('/process-examination').post(authController.protect, authController.restrictTo('admin', 'lab assistant'), labAssistantController.ProcessResult);
 router.route('/process-specimen').post(authController.protect, authController.restrictTo('admin', 'lab assistant'), labAssistantController.processSpecimen);
+router.route('/manage-profile').put(authController.protect, authController.restrictTo('admin', 'lab assistant'), labAssistantController.manageProfile);
 
 
 module.exports = router;
