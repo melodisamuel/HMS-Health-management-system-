721372de95ab97a93ace9cc6f4549d75a00e7c05
Melody Samuel <Melodysamuel126@gmail.com>
2024-03-06 15:02:36 +0100

    Implemented functionality for lab assisstant for manage own profile

--- controllers/labAssistantController.js
+++ controllers/labAssistantController.js
@@ -2,6 +2,7 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const Examination = require('../models/examinationResults');
 const LabAssistantSpecimen = require('../models/processSpecimen');
+const Registration = require('../models/registration');
 
 
 exports.ProcessResult = catchAsync(async (req, res, next) => {
@@ -27,4 +28,29 @@ exports.processSpecimen = catchAsync(async (req, res, next) => {
         }
     })
 })
+
+
+exports.manageProfile = catchAsync(async (req, res, next) => {
+    const { id } = req.params; // Correctly extract the id from req.params
+
+    // Find and update the profile of the specific doctor using the id
+    const profile = await Registration.findOneAndUpdate({ _id: id, role: 'lab assistant' }, req.body, {
+        new: true, // Return the updated document
+        runValidators: true // Run validation checks on the update operation
+    });
+
+    // Check if the profile was found and updated
+    if (!profile) {
+        return next(new AppError('There is no lab assistant with that profile', 404));
+    }
+
+    // Send the updated profile in the response
+    res.status(200).json({
+        status: "success",
+        message: "Profile updated successfully",
+        data: {
+            profile,
+        }
+    });
+});
 
