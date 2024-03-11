a826d7c5d3c00fe3e4f80b8dc0ed5a2f67c5ec0d
Melody Samuel <Melodysamuel126@gmail.com>
2024-03-11 16:16:58 +0100

    created a model for pharmacist to give medication

---
+++ models/medication.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const MedicationDispenseSchema = new mongoose.Schema({
+    prescription: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Prescription',
+        required: true,
+    },
+    patient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Patient',
+        required: true,
+    },
+    medicationGiven: {
+        type: String,
+        required: true,
+    },
+    quantityGiven: {
+        type: Number,
+        required: true,
+    },
+    dispensedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Pharmacist',
+        required: true,
+    },
+    dispensingDate: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const MedicationDispense = mongoose.model('MedicationDispense', MedicationDispenseSchema);
+
+module.exports = MedicationDispense;
