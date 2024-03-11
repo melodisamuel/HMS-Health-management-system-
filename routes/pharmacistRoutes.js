48bf8f893e5e233c8845591a79bbcfc652ac17ab
Melody Samuel <Melodysamuel126@gmail.com>
2024-03-11 16:48:08 +0100

    Hanled the route for pharmacist to dispense medication

--- routes/pharmacistRoutes.js
+++ routes/pharmacistRoutes.js
@@ -6,4 +6,6 @@ const router = express.Router();
 
 router.route('/watch-patient-prescription').get(authController.protect, authController.restrictTo('admin', 'pharmacist'), pharmacistController.watchPatientPrescription);
 
+router.route('/dispense-medication').post(authController.protect, authController.restrictTo('admin', 'pharmacist'), pharmacistController.DispenseMedication);
+
 module.exports = router;
